850125bd64c56c6037a0e7f794d2ad9c40b2ec52
Flavia Xie-Ku <fuyah@me.com>
2013-07-19 01:22:05 +1000

    Initial commit

---
+++ header.php
@@ -0,0 +1,67 @@
+<!doctype html>
+
+<!--[if lt IE 7]><html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if (IE 7)&!(IEMobile)]><html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8"><![endif]-->
+<!--[if (IE 8)&!(IEMobile)]><html <?php language_attributes(); ?> class="no-js lt-ie9"><![endif]-->
+<!--[if gt IE 8]><!--> <html <?php language_attributes(); ?> class="no-js"><!--<![endif]-->
+
+	<head>
+		<meta charset="utf-8">
+
+		<!-- Google Chrome Frame for IE -->
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+		<title><?php wp_title(''); ?></title>
+
+		<!-- mobile meta (hooray!) -->
+		<meta name="HandheldFriendly" content="True">
+		<meta name="MobileOptimized" content="320">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+		<!-- icons & favicons (for more: http://www.jonathantneal.com/blog/understand-the-favicon/) -->
+		<link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/library/images/apple-icon-touch.png">
+		<link rel="icon" href="<?php echo get_template_directory_uri(); ?>/favicon.png">
+		<!--[if IE]>
+			<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
+		<![endif]-->
+		<!-- or, set /favicon.ico for IE10 win -->
+		<meta name="msapplication-TileColor" content="#f01d4f">
+		<meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/library/images/win8-tile-icon.png">
+
+		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+
+		<!-- wordpress head functions -->
+		<?php wp_head(); ?>
+		<!-- end of wordpress head -->
+
+		<!-- drop Google Analytics Here -->
+		<!-- end analytics -->
+
+		<!-- enabling web fonts --> 
+
+		<script type="text/javascript" src="//use.typekit.net/rqq5jpe.js"></script>
+		<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+
+
+	</head>
+
+	<body <?php body_class(); ?>>
+
+		<div id="container">
+
+			<header class="header" role="banner">
+
+				<div id="inner-header" class="wrap clearfix">
+
+					<nav role="navigation">
+						<div id="masthead">
+							<h1 class="logo"><?php bloginfo('name'); ?></h1>
+						</div>
+						<?php bones_main_nav(); ?>
+					</nav>
+
+				</div> <!-- end #inner-header -->
+
+			</header> <!-- end header -->
+
+
---
+++ landingpage.php
@@ -0,0 +1,36 @@
+<?php
+/*
+Template Name: landingpage
+*/
+?>
+
+<?php get_header(); ?>
+
+			<div id="content">
+
+				<div id="inner-content" class="wrap clearfix">
+
+					<div class="img_block"></div>
+
+							<div class= "text">
+
+								<div class="under">
+
+									<div class="under_contents">
+
+										<h1> About Me </h1> 
+										<p> random inf </p> 
+
+									</div> 
+
+								</div> 
+
+							</div>  
+
+						</div> 
+
+				</div> <!-- end #inner-content -->
+
+			</div> <!-- end #content -->
+
+<?php get_footer(); ?>
