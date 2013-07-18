7713dde3c484bc2d8a84e96a22e126dbba0d506f
Flavia Xie-Ku <fuyah@me.com>
2013-07-19 09:58:56 +1000

    Home
    Added a blog page, trying to make own template

---
+++ blog.php
@@ -0,0 +1,35 @@
+<?php
+/*
+Template Name: blogpage
+*/
+?>
+
+<?php get_header(); ?>
+
+			<div id="content">
+
+				<div id="inner-content" class="wrap clearfix">
+
+					<div class="img_block"></div>
+
+							<div class= "text">
+
+								<div class="under">
+
+									<div class="under_contents">
+
+									<?php echo do_shortcode("[ic_add_posts]"); ?>
+
+									</div> 
+
+								</div> 
+
+							</div>  
+
+						</div> 
+
+				</div> <!-- end #inner-content -->
+
+			</div> <!-- end #content -->
+
+<?php get_footer(); ?>
